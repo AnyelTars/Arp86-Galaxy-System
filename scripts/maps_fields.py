2542c257fffaa865fa892bcb6eee57913607379e
Angel <angel.salazar@yachaytech.edu.ec>
2025-06-22 14:36:36 -0500

    update

--- scripts/maps_fields.py
+++ scripts/maps_fields.py
@@ -132,9 +132,9 @@ class Galaxy_maps:
         self.hdul_new2 = fits.PrimaryHDU(self.moment2_data, header=header_new) 
         self.hdul_new2.writeto(self.path_directory+'{}_.fits'.format(f'{name_galaxy}_moment2'),overwrite=True)
         print(f'data cubes sucessfully saved in {self.path_directory} as: ')
-        print(f'{self.path_directory+'{}_.fits'.format(f'{name_galaxy}_moment0')}')
-        print(f'{self.path_directory+'{}_.fits'.format(f'{name_galaxy}_moment1')}')
-        print(f'{self.path_directory+'{}_.fits'.format(f'{name_galaxy}_moment2')}')
+        print(f'{'{}_.fits'.format(f'{name_galaxy}_moment0')}')
+        print(f'{'{}_.fits'.format(f'{name_galaxy}_moment1')}')
+        print(f'{'{}_.fits'.format(f'{name_galaxy}_moment2')}')
 
         self.name_mom0 = '{}_.fits'.format(f'{name_galaxy}_moment0')
         self.name_mom1 = '{}_.fits'.format(f'{name_galaxy}_moment1')
@@ -162,7 +162,7 @@ class Galaxy_maps:
         header_mom1 = fits.open(self.path_directory+self.name_mom1)[0].header
         fig = plt.figure(figsize=(7,6))
         ax = fig.add_subplot(1,1,1,projection = (WCS(header_mom1)))
-        cb = plt.imshow(self.moment1_data,cmap='RdBu_r',vmin=4757.7983,vmax=5400)
+        cb = plt.imshow(self.moment1_data,cmap='RdBu_r')
         ax.set_xlabel('RA')
         ax.set_ylabel('Dec')
         cbar = fig.colorbar(cb)
@@ -185,7 +185,7 @@ class Galaxy_maps:
         plt.show()
 
     def plot_overlays(self):
-
+        
         # prepare the data 
         hdul_mom0 = fits.open(self.path_directory+self.name_mom0)
         hdul_mom1 = fits.open(self.path_directory+self.name_mom1)
@@ -193,23 +193,34 @@ class Galaxy_maps:
         header_mom0 = hdul_mom0[0].header
         header_mom1 = hdul_mom1[0].header
         header_mom2 = hdul_mom2[0].header
-
-
+        
+        #
+    
         data_optical,header_optical = self.read_files(type="optical")
         # moment 0
         hi_reprojected_0,footprint_0 = reproject_interp(hdul_mom0,header_optical)
         rms_mom0 = np.sqrt(np.nanmean(hi_reprojected_0**2))
+        m0max = np.nanmax(hi_reprojected_0)
+        m0min = np.nanmin(hi_reprojected_0) 
+        steps_0 = (m0max-m0min)*5/100
         #moment 1 
         hi_reprojected_1,footprint_1 = reproject_interp(hdul_mom1,header_optical)
+        m1max = np.nanmax(hi_reprojected_1)
+        m1min = np.nanmin(hi_reprojected_1) 
+        steps_1 = (m1max-m1min)*5/100
 
         #moment 2
         hi_reprojected_2,footprint_2 = reproject_interp(hdul_mom2,header_optical)
+        m2max = int(np.ceil(np.nanmax(hi_reprojected_2)))
+        m2min = int(np.nanmin(hi_reprojected_2))
+        steps_2 = (m2max-m2min)*5/100
 
         # plot overlay integrated intensity and optical image
         fig=plt.figure(figsize=(9,8))
         ax = fig.add_subplot(1,1,1, projection=WCS(header_mom0))
         cb=ax.imshow(data_optical,cmap='gist_heat',vmax=np.percentile(data_optical,99.8))
-        ax.contour(hi_reprojected_0,levels=np.arange(0,2.8,0.2)*rms_mom0,cmap='Blues_r')
+        #ax.contour(hi_reprojected_0,levels=np.arange(0,2.8,0.2)*rms_mom0,cmap='Blues_r')
+        ax.contour(hi_reprojected_0,levels=np.arange(m0min,m0max,steps_0),cmap='Blues_r')
         ax.set_xlabel('RA', size=18,family='serif')
         ax.set_ylabel('Dec', size=18,family='serif')
         plt.xticks(fontsize=15)
@@ -220,7 +231,7 @@ class Galaxy_maps:
         fig = plt.figure(figsize=(8,7))
         ax = fig.add_subplot(1,1,1, projection=WCS(header_mom1))
         ax.imshow(data_optical, cmap='gist_gray',vmax=np.percentile(data_optical, 99.8))
-        ax.contour(hi_reprojected_1, levels=np.arange(4800,5300,25),cmap='RdBu_r')
+        ax.contour(hi_reprojected_1, levels=np.arange(m1min,m1max,steps_1),cmap='RdBu_r')
         ax.set_xlabel('RA', size=18,family='serif')
         ax.set_ylabel('Dec', size=18,family='serif')
         plt.xticks(fontsize=15)
@@ -231,7 +242,7 @@ class Galaxy_maps:
         fig = plt.figure(figsize=(8,7))
         ax = fig.add_subplot(1,1,1, projection=WCS(header_mom2))
         ax.imshow(data_optical, cmap='gist_heat',vmax=np.percentile(data_optical, 99.8))
-        ax.contour(hi_reprojected_2, levels=np.arange(0,int(np.ceil(np.nanmax(hi_reprojected_2))),10),cmap='viridis')
+        ax.contour(hi_reprojected_2, levels=np.arange(m2min, m2max,steps_2),cmap='viridis')
         ax.set_xlabel('RA', size=18,family='serif')
         ax.set_ylabel('Dec', size=18,family='serif')
         plt.xticks(fontsize=15)
